03bd58ec00a372b7a8e134bbb0e8fc0220d0f815
leoyin84 <ljyin01@bvsd.org>
2025-08-20 18:56:42 +0000

    Added menu

--- Conversions.java
+++ Conversions.java
@@ -1,1 +1,15 @@
+// Leo Yin
+// 08/20/2025
 
+public class Conversions
+{
+    public static void main(String[] args)
+    {
+        System.out.println("1. Fahrenheit to Celsius");
+        System.out.println("2. Celsius to Fahrenheit");
+        System.out.println("3. Feet to Meters");
+        System.out.println("4. Meters to Feet");
+        System.out.println("5. Ounces to Milliters");
+        System.out.println("6. Milliters to Ounces");
+    }
+}
